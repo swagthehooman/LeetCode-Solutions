02ac7c83382960b892f353b24f79793c2580abc7
Swagnik Das <104345028+swagthehooman@users.noreply.github.com>
2022-11-01 23:20:20 +0530

    Time: 1 ms (96.80%), Space: 54.3 MB (52.71%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] findBall(int[][] grid) {
+        int result[]=new int[grid[0].length];
+        for(int i=0;i<grid[0].length;i++)
+            result[i]=findBallDrop(0,i,grid);
+        return result;
+    }
+    private int findBallDrop(int row, int col, int[][] grid){
+        if(row==grid.length)
+            return col;
+        
+        int nextCol=col+grid[row][col];
+        if(nextCol<0 || nextCol>grid[0].length-1 || grid[row][col]!=grid[row][nextCol])
+            return -1;
+        return findBallDrop(row+1,nextCol, grid);
+    }
+}
