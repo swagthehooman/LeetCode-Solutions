2fb510690e78706272b7d6c97bba509380af0a8e
Swagnik Das <104345028+swagthehooman@users.noreply.github.com>
2022-11-12 18:41:21 +0530

    Time: 33 ms (73.81%), Space: 50 MB (58.08%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        StringBuilder st=new StringBuilder();
+        for(char c : s.toCharArray()){
+            if(st.length()>0 && st.charAt(st.length()-1)==c)
+                st.deleteCharAt(st.length()-1);
+            else
+                st.append(c);
+        }
+        return st.toString();
+    }
+}
