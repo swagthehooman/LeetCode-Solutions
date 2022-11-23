32688937bf6d0252fda72f1d95313b92350dd7af
Swagnik Das <104345028+swagthehooman@users.noreply.github.com>
2022-11-23 09:59:49 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (13.24%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        int mem[]=new int[n+1];
+        mem[0]=0; mem[1]=1;
+        for(int i=2;i<=n;i++){
+            mem[i]=mem[i-1]+mem[i-2];
+        }
+        return mem[n];
+    }
+}
